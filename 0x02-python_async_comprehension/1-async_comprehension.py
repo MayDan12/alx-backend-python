3fdc72efafcda9e2098c1861eb7d4bda8481dc2d
MayDan12 <oluwamayowadan@gmail.com>
2024-05-07 18:46:06 +0000

    2-runtime.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -11,7 +11,7 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension():
     """
         The coroutine will collect 10 random numbers using
-        an async comprehensing over async_generator, 
+        an async comprehensing over async_generator,
         then return the 10 random numbers.
     """
     random_numbers = [number async for number in async_generator()]
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,26 @@
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    coroutine that will execute async_comprehension four times in
+    parallel using asyncio.gather.
+    measure_runtime should measure the total runtime and return it.
+    """
+    start_time = time.time()  # Record the start time
+
+    # Execute async_comprehension four times in parallel
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end_time = time.time()  # Record the end time
+
+    total_runtime = end_time - start_time  # Calculate the total runtime
+
+    return (total_runtime)
